dc99a90fd5682b6157708cc80d11bbf7bb3a2117
bishal692002 <105965588+bishal692002@users.noreply.github.com>
2025-04-03 12:01:49 +0530

    Time: 3 ms (89.27%) | Memory: 13.1 MB (41.61%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        hmap = {}
+        for num in nums:
+            if num in hmap:
+                hmap[num] += 1
+            else:
+                hmap[num] = 1
+        result = []
+        for key,value in hmap.items():
+            if value > (len(nums)//3):
+                result.append(key)
+        return result
