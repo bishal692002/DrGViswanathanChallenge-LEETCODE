48121e999b10516549827c3921f2937c7d928522
bishal692002 <105965588+bishal692002@users.noreply.github.com>
2025-04-08 20:41:43 +0530

    Time: 22 ms (63.45%) | Memory: 16.2 MB (64.88%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = {}
+        for word in strs:
+            sorte = "".join(sorted(word))
+            if sorte in anagrams:
+                anagrams[sorte].append(word)
+            else:
+                anagrams[sorte] = [word]
+        return anagrams.values()
+
+        
