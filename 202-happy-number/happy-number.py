61ed24073ae62323946b2099df839f375fbc6d62
bishal692002 <105965588+bishal692002@users.noreply.github.com>
2025-04-28 12:28:59 +0530

    Time: 4 ms (48.44%) | Memory: 12.5 MB (52.00%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def isHappy(self, n):
-        if n == 1 or n == 7: return True
-        elif n < 10: return False
-
-        s = 0
-        while n > 0:
-            r = n % 10
-            s += r ** 2 
-            n = n // 10
+        seen = set()
         
-        return self.isHappy(s)
+        while n != 1 and n not in seen:
+            seen.add(n)
+            s = 0
+            while n > 0:
+                r = n % 10
+                s += r ** 2
+                n = n // 10
+            n = s
         
-        
+        return n == 1
