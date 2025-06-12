c69fe1c683cc37371ee6ef0f588ba708b681f999
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-06-12 15:07:07 +0530

    Time: 7 ms (6.23%) | Memory: 12.5 MB (50.80%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -1,15 +1,12 @@
 class Solution(object):
     def finalValueAfterOperations(self, operations):
-        """
-        :type operations: List[str]
-        :rtype: int
-        """
-        value = 0
-        for i in range(len(operations)):
-            ip = operations[i]
-            if ip == "--X" or ip == "X--":
-                value -= 1
-            
-            elif ip == "++X" or ip=="X++":
-                value += 1
-        return value
+        X = 0
+        for i in operations:
+            if i == '--X' or i == 'X--':
+                X -= 1
+            elif i == '++X' or i == 'X++':  
+                X += 1
+        return X
+
+
+        
