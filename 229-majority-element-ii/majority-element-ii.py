98263c7786a89f13372bc2941d6fc3fe05333640
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-06-20 11:40:55 +0530

    Time: 6 ms (86.98%) | Memory: 13.7 MB (85.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -4,15 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        hmap = {}
+        if not nums:
+            return []
+        # There can be at most 2 elements > n//3 times
+        candidate1, candidate2, count1, count2 = None, None, 0, 0
         for num in nums:
-            if num in hmap:
-                hmap[num] += 1
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
             else:
-                hmap[num] = 1
+                count1 -= 1
+                count2 -= 1
+        # Verify the candidates
         result = []
-        for key,value in hmap.items():
-            if value > len(nums)//3:
-                result.append(key)
+        for c in (candidate1, candidate2):
+            if c is not None and nums.count(c) > len(nums) // 3:
+                result.append(c)
         return result
-        
