58823d8d556b6c774c89178c63d49b496e398ee3
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-07-28 18:58:59 +0530

    Time: 13 ms (72.45%) | Memory: 12.7 MB (80.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,13 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = s.lower()
-        original = ""
-        reverse = ""
+        l = 0
+        r = len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+        return True
+    
 
-        for char in s:
-            if char.isalnum():
-                original = original + char
-                reverse = char + reverse
-
-        return original == reverse
+        
