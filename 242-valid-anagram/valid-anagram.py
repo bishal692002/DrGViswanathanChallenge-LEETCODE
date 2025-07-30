e0a54d64b7cffdced960b5bac92586cb841c3ddb
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-07-30 17:33:18 +0530

    Time: 15 ms (85.39%) | Memory: 12.6 MB (83.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,15 +8,10 @@ class Solution(object):
         s1 = {}
         t1 = {}
         for has in s:
-            if has in s1:
-                s1[has] += 1
-            else:
-                s1[has] = 1
+            s1[has] = s1.get(has,0)+1
+            
         for has in t:
-            if has in t1:
-                t1[has] += 1
-            else:
-                t1[has] = 1
+            t1[has] = t1.get(has,0)+1
         return s1 == t1
         
         
