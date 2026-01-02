9e3d7ee783d11b07a1078c182a8190cc96a0c7df
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2026-01-02 16:57:20 +0530

    Time: 19 ms (66.32%) | Memory: 12.7 MB (66.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,15 +5,14 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+
+        if len(s) != len(t):
+            return False
         s1 = {}
         t1 = {}
-        for has in s:
-            s1[has] = s1.get(has,0)+1
-            
-        for has in t:
-            t1[has] = t1.get(has,0)+1
-        return s1 == t1
-        
-        
 
-        
+        for num in s:
+            s1[num] = s1.get(num,0)+1 
+        for num in t:
+            t1[num] = t1.get(num,0)+1 
+        return s1 == t1
