a18b5b636daffdfc5b22b740bd7680876cadfd1d
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2026-01-17 20:54:17 +0530

    Time: 51 ms (88.84%) | Memory: 42.2 MB (97.70%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,28 +1,17 @@
 class Solution(object):
     def rearrangeArray(self, nums):
         n = len(nums)
-        
-        pos = [0] * (n // 2)
-        neg = [0] * (n // 2)
-        
-        p = 0
-        q = 0
-        
-        # Separate positives and negatives
-        for num in nums:
-            if num < 0:
-                neg[q] = num
-                q += 1
-            else:
-                pos[p] = num
-                p += 1
-        
-        # Build result
         res = [0] * n
-        idx = 0
-        for i in range(p):
-            res[idx] = pos[i]
-            res[idx + 1] = neg[i]
-            idx += 2
+        
+        pos_idx = 0
+        neg_idx = 1
+        
+        for num in nums:
+            if num > 0:
+                res[pos_idx] = num
+                pos_idx += 2
+            else:
+                res[neg_idx] = num
+                neg_idx += 2
         
         return res
